f0cadc0c74df789548b7ba6f07ca518fe293bbc8
Elena Pavlova <epavlova@MacBook-Pro-Roman.local>
2018-11-04 13:58:47 +0300

    Created start project template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './main.css'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReacDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Router, Route, browserHistory} from 'react-router'
+import { Provider } from 'react-redux'
+
+import reducers from './reducers'
+import Layout from './containers/Layout'
+import Phones from './containers/Phones'
+
+console.log(`Layout ${Layout}`)
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+console.log(`store ${store}`)
+console.log(`browserHistory ${browserHistory}`)
+
+
+const history = syncHistoryWithStore(browserHistory, store)
+console.log(`Phones ${JSON.stringify(Phones)}`)
+ 
+ReacDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
